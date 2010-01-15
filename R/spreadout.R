bc54589f48eb8c19d0704d2466203496e5078b82
Jim Lemon <jim@bitwrit.com.au>
2010-01-15 00:00:00 +0000

    version 1.0-2

--- R/spreadout.R
+++ R/spreadout.R
@@ -1,31 +1,54 @@
 spreadout<-function(x,mindist) {
  if(sum(!is.na(x)) < 2) return(x)
  xorder<-order(x)
- # get the diffs between the ordered series of x
- diffo<-diff(x[xorder])
- diffo<-diffo[!is.na(diffo)]
- lendiffo<-length(diffo)
- middif<-lendiffo%/%2
- if(middif == 0) middif<-1
- # are there any diffs less than mindist
- if(any(diffo < mindist)) {
-  # start from the middle, work out
-  for(xoind in middif:1) {
-   if(diffo[xoind] < mindist) x[xorder[xoind]]<-x[xorder[xoind+1]]-mindist
-   # check that this hasn't jumped over the next point
-   if(xoind > 1 && x[xorder[xoind-1]] > x[xorder[xoind]]) {
-    x[xorder[xoind-1]]<-x[xorder[xoind]]-mindist/10
-    # recalculate the diffs or it will miss this
-    diffo<-diff(x[xorder])
+ goodx<-x[xorder][!is.na(x[xorder])]
+ gxlen<-length(goodx)
+ start<-end<-gxlen%/%2
+ # nicely spread groups of short intervals apart from their mean
+ while(start > 0) {
+  while(end < gxlen && goodx[end+1] - goodx[end] < mindist) end<-end+1
+  while(start > 1 && goodx[start] - goodx[start-1] < mindist) start<-start-1
+  if(start < end) {
+   nsqueezed<-1+end-start
+   newx<-sum(goodx[start:end])/nsqueezed -
+    mindist*(nsqueezed%/%2 - (nsqueezed/2 == nsqueezed%/%2) * 0.5)
+   for(stretch in start:end) {
+    goodx[stretch]<-newx
+    newx<-newx+mindist
    }
   }
-  for(xoind in middif:lendiffo) {
-   if(diffo[xoind] < mindist) x[xorder[xoind+1]]<-x[xorder[xoind]]+mindist
-   if(xoind < lendiffo && x[xorder[xoind+2]] < x[xorder[xoind+1]]) {
-    x[xorder[xoind+2]]<-x[xorder[xoind+1]]+mindist/10
-    diffo<-diff(x[xorder])
+  start<-end<-start-1
+ }
+ start<-end<-length(goodx)%/%2+1
+ while(start < gxlen) {
+  while(start > 1 && goodx[start] - goodx[start-1] < mindist) start<-start-1
+  while(end < gxlen && goodx[end+1] - goodx[end] < mindist) end<-end+1
+  if(start < end) {
+   nsqueezed<-1+end-start
+   newx<-sum(goodx[start:end])/nsqueezed -
+    mindist*(nsqueezed%/%2 - (nsqueezed/2 == nsqueezed%/%2) * 0.5)
+   for(stretch in start:end) {
+    goodx[stretch]<-newx
+    newx<-newx+mindist
    }
+  }
+  start<-end<-end+1
+ }
+ # force any remaining short intervals apart
+ if(any(diff(goodx) < mindist)) {
+  start<-gxlen%/%2
+  while(start > 1) {
+   if(goodx[start] - goodx[start-1] < mindist)
+    goodx[start-1]<-goodx[start]-mindist
+   start<-start-1
+  }
+  end<-gxlen%/%2
+  while(end < gxlen) {
+   if(goodx[end+1] - goodx[end] < mindist)
+    goodx[end+1]<-goodx[end]+mindist
+   end<-end+1
   }
  }
+ x[xorder][!is.na(x[xorder])]<-goodx
  return(x)
 }
